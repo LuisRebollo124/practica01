c0fc05d685822155d6f8a8d99d24f3606d49d50b
Luis Rebollo <luisrebollo30@gmail.com>
2022-10-16 19:29:06 +0200

    Páginas y botón

---
+++ java.js
@@ -0,0 +1,22 @@
+let imgchange = document.getElementById("imgchange");
+let breda = document.getElementById("breda");
+let inocencio = document.getElementById("inocencio");
+let meninas = document.getElementById("meninas");
+let vulcano = document.getElementById("vulcano");
+let venus = document.getElementById("venus");
+
+breda.onclick = function() {
+    imgchange.src = "img/breda.jpg"
+}
+inocencio.onclick = function() {
+    imgchange.src = "img/inocencio.jpg"
+}
+meninas.onclick = function() {
+    imgchange.src = "img/meninas.jpg"
+}
+vulcano.onclick = function() {
+    imgchange.src = "img/vulcano.jpg"
+}
+venus.onclick = function() {
+    imgchange.src = "img/venus.jpg"
+}
